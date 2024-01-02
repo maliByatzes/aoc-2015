9e62b6f9064ff39fa9d2da832643f4478fac2993
maliByatzes <malib2027@gmail.com>
2024-01-02 23:03:24 +0200

    read input from file

--- day3/main.cpp
+++ day3/main.cpp
@@ -3,8 +3,10 @@
 // He thens moves either north (`^`), south (`v`), east (`>`) or west ('<').
 // After each move he dilivers a present at that current location.
 
+#include <fstream>
 #include <iostream>
 #include <set>
+#include <string>
 #include <utility>
 
 int main() {
@@ -23,37 +25,48 @@ int main() {
   // East move => (x+1,y)
   // West move => (x-1,y)
 
-  for (char c : dir_test) {
-    if (c == '^') {
-      curr_location.second += 1;
-      auto r_pair1 = locations.insert(curr_location);
-      if (!r_pair1.second) {
-        continue;
-      } else {
-        houses_delivered_to++;
-      }
-    } else if (c == 'v') {
-      curr_location.second -= 1;
-      auto r_pair2 = locations.insert(curr_location);
-      if (!r_pair2.second)
-        continue;
-      else
-        houses_delivered_to++;
-    } else if (c == '>') {
-      curr_location.first += 1;
-      auto r_pair3 = locations.insert(curr_location);
-      if (!r_pair3.second) {
-        continue;
-      } else {
-        houses_delivered_to++;
-      }
-    } else if (c == '<') {
-      curr_location.first -= 1;
-      auto r_pair4 = locations.insert(curr_location);
-      if (!r_pair4.second) {
-        continue;
-      } else {
-        houses_delivered_to++;
+  // Read input from file
+  std::ifstream file("input.txt");
+
+  if (!file.is_open()) {
+    std::cerr << "Unable to open file.\n";
+    return 1;
+  }
+
+  std::string line{};
+  while (std::getline(file, line)) {
+    for (char c : line) {
+      if (c == '^') {
+        curr_location.second += 1;
+        auto r_pair1 = locations.insert(curr_location);
+        if (!r_pair1.second) {
+          continue;
+        } else {
+          houses_delivered_to++;
+        }
+      } else if (c == 'v') {
+        curr_location.second -= 1;
+        auto r_pair2 = locations.insert(curr_location);
+        if (!r_pair2.second)
+          continue;
+        else
+          houses_delivered_to++;
+      } else if (c == '>') {
+        curr_location.first += 1;
+        auto r_pair3 = locations.insert(curr_location);
+        if (!r_pair3.second) {
+          continue;
+        } else {
+          houses_delivered_to++;
+        }
+      } else if (c == '<') {
+        curr_location.first -= 1;
+        auto r_pair4 = locations.insert(curr_location);
+        if (!r_pair4.second) {
+          continue;
+        } else {
+          houses_delivered_to++;
+        }
       }
     }
   }
